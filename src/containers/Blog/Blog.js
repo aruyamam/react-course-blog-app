3eb5a61d73def24db7ab86ce9f2f2ae8082902b3
aruyamam <aruyamam@gmail.com>
2018-07-01 12:39:58 +0900

    Section 11 Preparing the Project for Routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,56 +2,11 @@ import React, { Component } from 'react';
 // import axios from 'axios';
 import axios from '../../axios';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import Posts from './Posts/Posts';
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null,
-    error: false,
-  };
-
-  componentDidMount() {
-    axios.get('/posts')
-      .then(response => {
-        const posts = response.data.slice(0, 4);
-        const updatedPost = posts.map(post => {
-          return {
-            ...post,
-            author: 'Max',
-          };
-        });
-        this.setState({ posts: updatedPost });
-        // console.log(response);
-      })
-      .catch(error => {
-        // console.log(error);
-        this.setState({ error: true });
-      });
-  }
-
-  postSeletedHandler = (id) => {
-    this.setState({ selectedPostId: id });
-  };
-
   render() {
-    let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
-    if  (!this.state.error) {
-      posts = this.state.posts.map(post => {
-        return (
-          <Post
-            title={post.title}
-            author={post.author}
-            key={post.id}
-            clicked={() => this.postSeletedHandler(post.id)}
-          />
-        );
-      });
-    }
-
     return (
       <div>
         <header className="Header">
@@ -62,15 +17,7 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <section className="Posts">
-          {posts}
-        </section>
-        <section>
-          <FullPost id={this.state.selectedPostId} />
-        </section>
-        <section>
-          <NewPost />
-        </section>
+        <Posts />
       </div>
     );
   }
